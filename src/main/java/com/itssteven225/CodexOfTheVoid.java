6eaf248ecf02526381a4610ef290cd0b125cb9e6
ItsSteven225 <sup3rsteven1@gmail.com@ejemplo.com>
2025-10-14 23:07:36 -0500

    Initial commit

---
+++ src/main/java/com/itssteven225/CodexOfTheVoid.java
@@ -0,0 +1,16 @@
+package com.itssteven225;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CodexOfTheVoid implements ModInitializer {
+
+	public static final String MOD_ID = "codex-of-the-void";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
